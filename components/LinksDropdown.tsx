33aec1721bad22ccfc3ae1c45d1860eef9e6e1f9
Jesse Pancho <jesse.pancho@tkservices.co>
2026-04-07 14:40:54 +0800

    Working on Dropdown

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,7 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import React from "react";
+import { Button } from "./ui/button";
+import { AlignLeft } from "lucide-react";
+import links from "@/utils/links";
+import { link } from "fs";
+import Link from "next/link";
 
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="flex items-center gap-x-2">
+                {link.icon} <span className="capitalize">{link.label}</span>
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
+
+// Dropdownlinks Component, Change Theme and Toggle Theme at 6:05
