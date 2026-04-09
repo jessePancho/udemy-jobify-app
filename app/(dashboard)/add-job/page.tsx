6517cbca2ce8d954e2d0cca319de74bc0026d6ae
jessepancho <juicepancho007@gmail.com>
2026-04-09 16:47:22 +0800

    CreateJobForm

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,6 +1,11 @@
+import CreateJobForm from "@/components/CreateJobForm";
 
 const AddJobPage = () => {
-    return <h1 className="text-4xl">AddJobPage</h1>
-}
+  return (
+    <h1 className="text-4xl">
+      <CreateJobForm />
+    </h1>
+  );
+};
 
 export default AddJobPage;
---
+++ components/CreateJobForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { zodResolver } from "@hookform/resolvers";
+import { useForm } from "react-hook-form";
+import * as Z from "zod";
+
+import { Button } from "./ui/button";
+
+const CreateJobForm = () => {
+  return <div>Create Job Form</div>;
+};
+
+export default CreateJobForm;
